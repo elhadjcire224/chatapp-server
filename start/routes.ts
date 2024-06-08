838b8ddbbff10b7c250a0a1fab52cb02e3cd6c9a
elhadjcire224 <bahmamadoucire14@gmail.com>
2024-06-08 21:42:30 +0000

    feat(app): enable routing for auth the feature
    writing routes for authentification feature without implementing OAuth authentification

--- start/routes.ts
+++ start/routes.ts
@@ -7,10 +7,15 @@
 |
 */
 
+const AuthController =  () => import('#controllers/auth_controller')
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
+router.group(() => {
+  router.post("/register", [AuthController,'register']).as('register')
+  router.post("/login",[AuthController,'login']).as('login')
+  router.delete('/logout',[AuthController,"logout"]).use(middleware.auth()).as("logout")
+}).prefix('auth')
+
+
+router.get("/me",[AuthController,'me']).as("me").use(middleware.auth())
