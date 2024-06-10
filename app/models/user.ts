78224619aa853ee9450ff122407acee7c328499f
elhadjcire224 <bahmamadoucire14@gmail.com>
2024-06-10 23:12:14 +0000

    feat(app): change fullname field to username

--- app/models/user.ts
+++ app/models/user.ts
@@ -15,7 +15,10 @@ export default class User extends compose(BaseModel, AuthFinder) {
   declare id: number
 
   @column()
-  declare fullName: string | null
+  declare username: string | null
+
+  @column()
+  declare avatarUrl: string | null
 
   @column()
   declare email: string
